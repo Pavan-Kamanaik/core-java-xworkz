bcf052220ad4fbd80c8ee068b675cb74b819ea89
Pavan-Kamanaik <pavank.xworkz@gmail.com>
2023-08-21 11:49:13 +0530

    Eat fit items

---
+++ EatFit.java
@@ -0,0 +1,423 @@
+class EatFit {
+    public static double ordFood(String itemName) {
+        double costIn$ = 0.0;
+        System.out.println("EatFit order started");
+
+        if (itemName == "GrilledChickenSalad") {
+            costIn$ = 8.99;
+            return costIn$;
+        }
+        if (itemName == "QuinoaBowl") {
+            costIn$ = 7.49;
+            return costIn$;
+        }
+        if (itemName == "AvocadoToast") {
+            costIn$ = 6.99;
+            return costIn$;
+        }
+        if (itemName == "TofuWrap") {
+            costIn$ = 7.79;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtParfait") {
+            costIn$ = 5.99;
+            return costIn$;
+        }
+        if (itemName == "VeggieSmoothie") {
+            costIn$ = 4.49;
+            return costIn$;
+        }
+        if (itemName == "SalmonWithVeggies") {
+            costIn$ = 10.99;
+            return costIn$;
+        }
+        if (itemName == "EggWhiteOmelette") {
+            costIn$ = 6.59;
+            return costIn$;
+        }
+        if (itemName == "HummusPlatter") {
+            costIn$ = 5.29;
+            return costIn$;
+        }
+        if (itemName == "FruitSalad") {
+            costIn$ = 3.99;
+            return costIn$;
+        }
+        if (itemName == "BrownRiceBowl") {
+            costIn$ = 7.89;
+            return costIn$;
+        }
+        if (itemName == "LeanTurkeyBurger") {
+            costIn$ = 8.79;
+            return costIn$;
+        }
+        if (itemName == "VeganWrap") {
+            costIn$ = 6.99;
+            return costIn$;
+        }
+        if (itemName == "ChiaPudding") {
+            costIn$ = 4.99;
+            return costIn$;
+        }
+        if (itemName == "MixedNuts") {
+            costIn$ = 3.49;
+            return costIn$;
+        }
+        if (itemName == "ZucchiniNoodles") {
+            costIn$ = 6.29;
+            return costIn$;
+        }
+        if (itemName == "SalmonSteak") {
+            costIn$ = 11.99;
+            return costIn$;
+        }
+        if (itemName == "TofuStirFry") {
+            costIn$ = 9.79;
+            return costIn$;
+        }
+        if (itemName == "FreshFruitSmoothie") {
+            costIn$ = 5.99;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtParfait") {
+            costIn$ = 4.49;
+            return costIn$;
+        }
+        if (itemName == "KaleSalad") {
+            costIn$ = 6.79;
+            return costIn$;
+        }
+        if (itemName == "MixedBerriesBowl") {
+            costIn$ = 5.29;
+            return costIn$;
+        }
+        if (itemName == "VeganBurger") {
+            costIn$ = 6.79;
+            return costIn$;
+        }
+        if (itemName == "GrilledSalmonBowl") {
+            costIn$ = 10.49;
+            return costIn$;
+        }
+        if (itemName == "MushroomRisotto") {
+            costIn$ = 9.29;
+            return costIn$;
+        }
+        if (itemName == "AlmondMilkSmoothie") {
+            costIn$ = 5.79;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtBowl") {
+            costIn$ = 4.99;
+            return costIn$;
+        }
+        if (itemName == "EggAvocadoToast") {
+            costIn$ = 6.29;
+            return costIn$;
+        }
+        if (itemName == "SpinachSalad") {
+            costIn$ = 5.49;
+            return costIn$;
+        }
+        if (itemName == "AcaiBowl") {
+            costIn$ = 7.79;
+            return costIn$;
+        }
+        if (itemName == "MangoSalsaChicken") {
+            costIn$ = 10.99;
+            return costIn$;
+        }
+        if (itemName == "AvoTunaSalad") {
+            costIn$ = 9.49;
+            return costIn$;
+        }
+        if (itemName == "BerrySpinachSmoothie") {
+            costIn$ = 5.99;
+            return costIn$;
+        }
+        if (itemName == "QuinoaStuffedPeppers") {
+            costIn$ = 9.79;
+            return costIn$;
+        }
+        if (itemName == "HummusPlatter") {
+            costIn$ = 8.49;
+            return costIn$;
+        }
+        if (itemName == "EggPlantSalad") {
+            costIn$ = 6.99;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtSmoothie") {
+            costIn$ = 5.29;
+            return costIn$;
+        }
+        if (itemName == "SpaghettiSquash") {
+            costIn$ = 8.79;
+            return costIn$;
+        }
+        if (itemName == "SpinachSalad") {
+            costIn$ = 5.49;
+            return costIn$;
+        }
+        if (itemName == "AcaiBowl") {
+            costIn$ = 7.79;
+            return costIn$;
+        }
+        // Add more menu items
+        if (itemName == "CauliflowerRiceBowl") {
+            costIn$ = 7.99;
+            return costIn$;
+        }
+        if (itemName == "ChiaPudding") {
+            costIn$ = 4.79;
+            return costIn$;
+        }
+        if (itemName == "MushroomRisotto") {
+            costIn$ = 9.29;
+            return costIn$;
+        }
+        if (itemName == "AlmondMilkSmoothie") {
+            costIn$ = 5.79;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtBowl") {
+            costIn$ = 4.99;
+            return costIn$;
+        }
+        if (itemName == "EggAvocadoToast") {
+            costIn$ = 6.29;
+            return costIn$;
+        }
+        if (itemName == "LemonHerbGrilledFish") {
+            costIn$ = 10.99;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtFruitParfait") {
+            costIn$ = 5.99;
+            return costIn$;
+        }
+        if (itemName == "BroccoliSalad") {
+            costIn$ = 6.79;
+            return costIn$;
+        }
+        if (itemName == "BerryChiaSmoothie") {
+            costIn$ = 5.49;
+            return costIn$;
+        }
+
+        System.out.println("EatFit order ended");
+
+        return costIn$;
+    }
+
+     public static double ordFood(String itemName,int quantity) {
+        double costIn$ = 0.0;
+        System.out.println("EatFit order started");
+
+        if (itemName == "GrilledChickenSalad") {
+            costIn$ = 8.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "QuinoaBowl") {
+            costIn$ = 7.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "AvocadoToast") {
+            costIn$ = 6.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "TofuWrap") {
+            costIn$ = 7.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtParfait") {
+            costIn$ = 5.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "VeggieSmoothie") {
+            costIn$ = 4.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "SalmonWithVeggies") {
+            costIn$ = 10.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "EggWhiteOmelette") {
+            costIn$ = 6.59*quantity;
+            return costIn$;
+        }
+        if (itemName == "HummusPlatter") {
+            costIn$ = 5.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "FruitSalad") {
+            costIn$ = 3.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "BrownRiceBowl") {
+            costIn$ = 7.89*quantity;
+            return costIn$;
+        }
+        if (itemName == "LeanTurkeyBurger") {
+            costIn$ = 8.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "VeganWrap") {
+            costIn$ = 6.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "ChiaPudding") {
+            costIn$ = 4.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "MixedNuts") {
+            costIn$ = 3.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "ZucchiniNoodles") {
+            costIn$ = 6.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "SalmonSteak") {
+            costIn$ = 11.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "TofuStirFry") {
+            costIn$ = 9.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "FreshFruitSmoothie") {
+            costIn$ = 5.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtParfait") {
+            costIn$ = 4.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "KaleSalad") {
+            costIn$ = 6.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "MixedBerriesBowl") {
+            costIn$ = 5.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "VeganBurger") {
+            costIn$ = 6.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "GrilledSalmonBowl") {
+            costIn$ = 10.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "MushroomRisotto") {
+            costIn$ = 9.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "AlmondMilkSmoothie") {
+            costIn$ = 5.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtBowl") {
+            costIn$ = 4.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "EggAvocadoToast") {
+            costIn$ = 6.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "SpinachSalad") {
+            costIn$ = 5.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "AcaiBowl") {
+            costIn$ = 7.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "MangoSalsaChicken") {
+            costIn$ = 10.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "AvoTunaSalad") {
+            costIn$ = 9.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "BerrySpinachSmoothie") {
+            costIn$ = 5.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "QuinoaStuffedPeppers") {
+            costIn$ = 9.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "HummusPlatter") {
+            costIn$ = 8.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "EggPlantSalad") {
+            costIn$ = 6.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtSmoothie") {
+            costIn$ = 5.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "SpaghettiSquash") {
+            costIn$ = 8.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "SpinachSalad") {
+            costIn$ = 5.49*quantity;
+            return costIn$;
+        }
+        if (itemName == "AcaiBowl") {
+            costIn$ = 7.79*quantity;
+            return costIn$;
+        }
+        // Add more menu items
+        if (itemName == "CauliflowerRiceBowl") {
+            costIn$ = 7.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "ChiaPudding") {
+            costIn$ = 4.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "MushroomRisotto") {
+            costIn$ = 9.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "AlmondMilkSmoothie") {
+            costIn$ = 5.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtBowl") {
+            costIn$ = 4.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "EggAvocadoToast") {
+            costIn$ = 6.29*quantity;
+            return costIn$;
+        }
+        if (itemName == "LemonHerbGrilledFish") {
+            costIn$ = 10.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "GreekYogurtFruitParfait") {
+            costIn$ = 5.99*quantity;
+            return costIn$;
+        }
+        if (itemName == "BroccoliSalad") {
+            costIn$ = 6.79*quantity;
+            return costIn$;
+        }
+        if (itemName == "BerryChiaSmoothie") {
+            costIn$ = 5.49*quantity;
+            return costIn$;
+        }
+
+        System.out.println("EatFit order ended");
+
+        return costIn$;
+    }
+}
