1e91f9d3846f8957b7aec2f2433026a1add89f70
Pavan-Kamanaik <pavank.xworkz@gmail.com>
2023-08-22 12:19:34 +0530

    For loop

---
+++ Netflix1.java
@@ -0,0 +1,320 @@
+class Netflix {
+    // KANNADA MOVIES
+    static String kannadaMovies[] = {
+            "Mumbai's Message",
+            "Yugadi",
+            "Mookanidi",
+            "Bul Bul Bul",
+            "Jodi",
+            "Ulidavaru Kandante",
+            "Manasu Mallige",
+            "Gandhadagudi",
+            "Love Matrix",
+            "Kranti Kala",
+            "KGF: Chapter 2",
+            "Pushpa: The Rise",
+            "777 Charlie",
+            "Beast",
+            "Yash 19",
+            "Salaar",
+            "Vikrant Rona",
+            "Love Story",
+            "Bheemasena Nalamaharaja",
+            "Avane Srimannarayana",
+            "KGF: Chapter 1",
+            "Pushpa: The Rule",
+            "83",
+            "RRR",
+            "The Kashmir Files",
+            "Gangubai Kathiawadi",
+
+    };
+    // ENGLISH MOVIES
+    static String englishSeries[] = {
+            "The Shawshank Redemption",
+            "The Godfather",
+            "The Dark Knight",
+            "Inception",
+            "Pulp Fiction",
+            "Forrest Gump",
+            "Fight Club",
+            "The Matrix",
+            "Interstellar",
+            "Avatar",
+            "Jurassic Park",
+            "Star Wars: Episode IV - A New Hope",
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "Harry Potter and the Sorcerer's Stone",
+            "Titanic",
+            "The Lion King",
+            "The Avengers",
+            "The Social Network",
+            "Gladiator",
+            "The Terminator",
+            "E.T. the Extra-Terrestrial",
+            "Indiana Jones and the Raiders of the Lost Ark",
+            "Jaws",
+            "The Great Gatsby",
+            "Mad Max: Fury Road",
+            "The Revenant"
+    };
+
+    // HINDI MOVIES
+    static String hindiMovies[] = {
+            "Sholay",
+            "Dilwale Dulhania Le Jayenge",
+            "3 Idiots",
+            "PK",
+            "Kabhi Khushi Kabhie Gham",
+            "Bajrangi Bhaijaan",
+            "Bahubali: The Beginning",
+            "Chennai Express",
+            "Lagaan",
+            "Golmaal: Fun Unlimited",
+            "My Name Is Khan",
+            "Kuch Kuch Hota Hai",
+            "Mughal-E-Azam",
+            "Dangal",
+            "Dabangg",
+    };
+
+    // teluguMovies
+    static String teluguMovies[] = {
+            "Baahubali: The Beginning",
+            "Baahubali 2: The Conclusion",
+            "Magadheera",
+            "Arjun Reddy",
+            "Ala Vaikunthapurramuloo",
+            "Rangasthalam",
+            "Sye",
+            "Jersey",
+            "Eega",
+            "Khaidi No. 150",
+            "Srimanthudu",
+            "Pokiri",
+            "Nannaku Prematho",
+            "Mirchi",
+            "Geetha Govindam",
+            "Attarintiki Daredi",
+            "Fidaa",
+            "Bahubali",
+            "Maharshi",
+            "Bharat Ane Nenu",
+            "Eega",
+            "Nuvvostanante Nenoddantana",
+            "I - Manoharudu",
+            "Arya",
+            "Pelli Choopulu"
+    };
+
+    static String malayalamMovies[] = {
+            "Drishyam",
+            "Premam",
+            "Bangalore Days",
+            "Kilukkam",
+            "Meesa Madhavan",
+            "Nayakan",
+            "Charlie",
+            "Ayalum Njanum Thammil",
+            "Ustad Hotel",
+            "Maheshinte Prathikaaram",
+            "Vadakkunokkiyantram",
+            "Manichitrathazhu",
+            "Amen",
+            "Pulimurugan",
+            "Kumbalangi Nights",
+            "Oppam",
+            "Kireedam",
+            "Banglavil Outha",
+            "Manu Uncle",
+            "Ramji Rao Speaking",
+            "Moonnam Pakkam",
+            "Thira",
+            "Pathemari",
+            "Spadikam",
+            "Godfather"
+    };
+
+    public static void main(String netflix[]) {
+        // kannadaMovies[25]
+        // englishMovies[25]
+        // hindiSeries[15]
+        // teluguMovies[25]
+        // malayalamMovies[25]
+
+        // KANNADA MOVIES
+        // System.out.println("Netflix Kannada Movies : " +kannadaMovies[0]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[1]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[2]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[3]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[4]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[5]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[6]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[7]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[8]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[9]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[10]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[11]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[12]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[13]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[14]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[15]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[16]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[17]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[18]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[19]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[20]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[21]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[22]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[23]);
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[24]);
+
+        // Instead of doing this SOP is we can save the time and energy we can use for
+        // loop
+
+
+        //Data from left to right 
+
+        // for (int index = 0; index < kannadaMovies.length; index++) {
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[index]);
+        // }
+
+          //Data from right to left 
+
+        // for (int index=kannadaMovies.length-1; index>=0; index--) {
+        // System.out.println("Netflix Kannada Movies : "+kannadaMovies[index]);
+        // }
+
+
+        // ENGLISH MOVIES
+        // System.out.println("Netflix English Series : "+englishSeries[0]);
+        // System.out.println("Netflix English Series : "+englishSeries[1]);
+        // System.out.println("Netflix English Series : "+englishSeries[2]);
+        // System.out.println("Netflix English Series : "+englishSeries[3]);
+        // System.out.println("Netflix English Series : "+englishSeries[4]);
+        // System.out.println("Netflix English Series : "+englishSeries[5]);
+        // System.out.println("Netflix English Series : "+englishSeries[6]);
+        // System.out.println("Netflix English Series : "+englishSeries[7]);
+        // System.out.println("Netflix English Series : "+englishSeries[8]);
+        // System.out.println("Netflix English Series : "+englishSeries[9]);
+        // System.out.println("Netflix English Series : "+englishSeries[10]);
+        // System.out.println("Netflix English Series : "+englishSeries[11]);
+        // System.out.println("Netflix English Series : "+englishSeries[12]);
+        // System.out.println("Netflix English Series : "+englishSeries[13]);
+        // System.out.println("Netflix English Series : "+englishSeries[14]);
+        // System.out.println("Netflix English Series : "+englishSeries[15]);
+        // System.out.println("Netflix English Series : "+englishSeries[16]);
+        // System.out.println("Netflix English Series : "+englishSeries[17]);
+        // System.out.println("Netflix English Series : "+englishSeries[18]);
+        // System.out.println("Netflix English Series : "+englishSeries[19]);
+        // System.out.println("Netflix English Series : "+englishSeries[20]);
+        // System.out.println("Netflix English Series : "+englishSeries[21]);
+        // System.out.println("Netflix English Series : "+englishSeries[22]);
+        // System.out.println("Netflix English Series : "+englishSeries[23]);
+        // System.out.println("Netflix English Series : "+englishSeries[24]);
+
+        // for (int index = 0; index < englishSeries.length; index++) {
+        // System.out.println("Netflix English Series : "+englishSeries[index]);
+        // }
+
+        //   for (int index=englishSeries.length-1; index>=0; index--) {
+        // System.out.println("Netflix English Series : "+englishSeries[index]);
+        // }
+
+        // HINDI MOVIES
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[0]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[1]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[2]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[3]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[4]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[5]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[6]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[7]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[8]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[9]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[10]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[11]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[12]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[13]);
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[14]);
+
+        // for (int index = 0; index < hindiMovies.length; index++) {
+        // System.out.println("Netflix Hindi Movies : " + hindiMovies[index]);
+        // }
+
+        //    for (int index=hindiMovies.length-1; index>=0; index--) {
+        // System.out.println("Netflix Hindi Movies : "+hindiMovies[index]);
+        // }
+
+        // TELUGU MOVIES
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[0]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[1]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[2]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[3]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[4]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[5]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[6]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[7]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[8]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[9]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[10]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[11]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[12]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[13]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[14]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[15]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[16]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[17]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[18]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[19]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[20]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[21]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[22]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[23]);
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[24]);
+
+        // for (int index = 0; index < teluguMovies.length; index++) {
+        // System.out.println("Netflix Telugu Movies : " + teluguMovies[index]);
+        // }
+
+        //    for (int index=teluguMovies.length-1; index>=0; index--) {
+        // System.out.println("Netflix Telugu Movies : "+teluguMovies[index]);
+        // }
+
+        // MALAYALAM MOVIES
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[1]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[0]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[2]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[3]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[4]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[5]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[6]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[7]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[8]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[9]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[10]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[11]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[12]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[13]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[14]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[15]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[16]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[17]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[18]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[19]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[20]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[21]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[22]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[23]);
+        // System.out.println("Netflix Malayalam Movies : "+malayalamMovies[24]);
+
+        // for (int index = 0; index < malayalamMovies.length; index++) {
+        //     System.out.println("Netflix Malayalam Movies : " + malayalamMovies[index]);
+        // }
+
+           for (int index=malayalamMovies.length-1; index>=0; index--) {
+        System.out.println("Netflix Malayalam Movies : "+malayalamMovies[index]);
+        }
+
+    }
+}
