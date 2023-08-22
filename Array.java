807c10f718067ccc3ba38206a83a03c3505c25cc
Pavan-Kamanaik <pavank.xworkz@gmail.com>
2023-08-22 12:33:35 +0530

    Arrays

---
+++ Array.java
@@ -0,0 +1,12 @@
+class Array {
+
+    // static String incredients[]={"Black pepper","Dry ginger","Cassia","Cardomom"};
+    static String englishMovies[] ={"avengers","Iron man"};
+;
+    
+    public static void main(String incr[]){
+        // System.out.println("Everest chai masala incredients : "+incredients[0]);
+        System.out.println(englishMovies[3]);
+
+    }
+}
